8f145649e73694802f9df4eb0124d537088f0fe4
TahmidShawn7 <md.tahmid022@gmail.com>
2026-05-26 21:47:19 +0600

    learn location api

---
+++ javascript/07_browser_environment/location/script.js
@@ -0,0 +1,26 @@
+// full URL
+window.location.href;
+
+// domain
+window.location.hostname;
+
+// path
+window.location.pathname;
+
+// protocol (http / https)
+window.location.protocol;
+
+// query string (?a=1)
+window.location.search;
+
+// hash (#section)
+window.location.hash;
+
+// go to new page
+window.location.assign();
+
+// replace URL (no back)
+window.location.replace("https://google.com");
+
+// reload page
+window.location.reload();
