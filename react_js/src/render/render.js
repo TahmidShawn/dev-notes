be9d30dbdd97f6b9cee4d3f594cbdfcdf02d99d2
TahmidShawn7 <md.tahmid022@gmail.com>
2026-06-11 10:06:48 +0600

    note render info

---
+++ react_js/src/render/render.js
@@ -0,0 +1,15 @@
+// 1. before npm run dev ----> only files exist nothing runs
+
+// 2. npm run dev -----→ vite starts a dev server
+
+// 3. browser requests app -----→ vite converts jsx to plain js
+
+// 4. browser receives js (not jsx)
+
+// 5. jS runs in browser (runtime starts)
+
+// 6. react runs like normal js functions
+
+// 7. react (normal js func) creates element objects + uses dom apis
+
+// 8. browser renders ui on screen
